3578993f2a025d44a64faaaf0185ffa114d59a81
rachmanj <rachmanj@gmail.com>
2021-01-14 13:56:47 +0800

    add npi qty di dashb npi

--- resources/views/dashboard/page_1/npi.blade.php
+++ resources/views/dashboard/page_1/npi.blade.php
@@ -10,6 +10,8 @@
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">Project</th>
+                <th scope="col" class="text-right">In</th>
+                <th scope="col" class="text-right">Out</th>
                 <th scope="col" class="text-right">Index</th>
                 <th></th>
               </tr>
@@ -18,6 +20,8 @@
               <tr>
                 <th scope="row">1</th>
                 <td>011C</td>
+                <td class="text-right">{{ number_format($npi_incoming_011, 2) }}</td>
+                <td class="text-right">{{ number_format($npi_outgoing_011, 2) }}</td>
                 @if ($npi_incoming_011 && $npi_outgoing_011)
                 <td class="text-right">{{ number_format($npi_incoming_011 / $npi_outgoing_011, 2) }}</td>
                 @else
@@ -28,6 +32,8 @@
               <tr>
                 <th scope="row">2</th>
                 <td>017C</td>
+                <td class="text-right">{{ number_format($npi_incoming_017, 2) }}</td>
+                <td class="text-right">{{ number_format($npi_outgoing_017, 2) }}</td>
                 @if ($npi_incoming_017 && $npi_outgoing_017)
                 <td class="text-right">{{ number_format($npi_incoming_017 / $npi_outgoing_017, 2) }}</td>
                 @else
@@ -38,6 +44,8 @@
               <tr>
                 <th scope="row">3</th>
                 <td>APS</td>
+                <td class="text-right">{{ number_format($npi_incoming_APS, 2) }}</td>
+                <td class="text-right">{{ number_format($npi_outgoing_APS, 2) }}</td>
                 @if ($npi_incoming_APS && $npi_outgoing_APS)
                 <td class="text-right">{{ number_format($npi_incoming_APS / $npi_outgoing_APS, 2) }}</td>
                 @else
@@ -48,6 +56,8 @@
               <tr>
                 <th scope="row"></th>
                 <th>Total</th>
+                <th class="text-right">{{ number_format($npi_incoming_all, 2) }}</th>
+                <th class="text-right">{{ number_format($npi_outgoing_all, 2) }}</th>
                 <td class="text-right">@if ($npi_incoming_all && $npi_outgoing_all)
                     {{ number_format($npi_incoming_all / $npi_outgoing_all, 2) }}
                 @endif</td>
